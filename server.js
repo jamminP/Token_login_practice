b0be2791cfe174716bb1e772f3caa360467be453
jamminP <parkjaemin08@gmail.com>
2025-06-27 15:19:06 +0900

    Token으로 유저정보 관리하기 - 박재민

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:5501",
+      "http://localhost:5501",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -41,18 +41,45 @@ app.post("/", (req, res) => {
     res.status(401).send("로그인 실패");
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
-    // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign({
+      user_id: userInfo.user_id,
+      user_name: userInfo.user_name,
+    },
+    secretKey,
+    { expiresIn: "10m"}
+  );
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
-    // 이곳에 코드를 작성하세요.
+    res.send(accessToken);
   }
 });
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-  // 이곳에 코드를 작성하세요.
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send("토큰이 없습니다.");
+  }
+
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  const token = authHeader.split(" ")[1];
+
+  try{
+    const decoded = jwt.verify(token, secretKey);
+    const user = users.find(el => el.user_id === decoded.user_id);
+
+    if(!user){
+      return res.status(404).send("유저 정보 없음");
+    }
+
+    res.send({
+      user_id: user.user_id,
+      user_name: user.user_name,
+      user_info: user.user_info
+    });
+  }catch(err){
+    res.status(401).send("토큰이 유효하지 않음.");
+  }
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
